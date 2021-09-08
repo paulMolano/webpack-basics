3dbc9a46376aff3a964b519286add571128beead
paulMolano <paulmolanoo@gmail.com>
2021-09-08 13:52:31 +0200

    imagesok

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 const webpack = require("webpack");
+const svgToMiniDataURI = require("mini-svg-data-uri");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerWebpackPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devServer: {
+    port: 3000,
+    compress: true,
+    hot: true,
+    open: true,
+  },
   entry: {
     main: path.resolve(__dirname, "./src/index.js"),
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
-
     filename: "[name].js",
+    assetModuleFilename: "images/[hash][ext][query]",
   },
   plugins: [
     new webpack.ProvidePlugin({
@@ -19,24 +27,63 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       title: "Webpack Basics",
-      template: path.resolve(__dirname, "./src/index.html"),
+      template: "./src/index.html",
       filename: "index.html",
+    }),
+    new ImageMinimizerWebpackPlugin({
+      minimizerOptions: {
+        plugins: ["gifsicle", "jpegtran", "optipng", "svgo"],
+      },
     }),
   ],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/i,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
       {
-        test: /\.(scss|css)$/,
+        test: /\.(scss|sass)$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        type: "asset/inline",
-        test: /\.(png|svg)$/i,
+        test: /\.(jpe?g|gif)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.png$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024, // 8kb
+          },
+        },
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 12 * 1024, // 12kb
+          },
+        },
+        generator: {
+          dataUrl: (content) => {
+            content = content.toString();
+            return svgToMiniDataURI(content);
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
